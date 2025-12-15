af75c7b3f258559c4685d4fc4a993ac1f0cb96d9
Guillaume <guillaume@sowinski.fr>
2025-12-15 16:18:40 +0100

    test2

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware pour parser le JSON
-app.use(express.json());
-
-const usersRouter = require('./routes/users');
-app.use('/users', usersRouter);
-
 // Route de test
 app.get('/', (req, res) => {
   res.send('Hello World !');
+});
+
+app.get('/status', (req, res) => {
+  res.send({ status: 'API is running' });
 });
 
 // Lancer le serveur
